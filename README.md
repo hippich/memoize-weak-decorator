e3f625fb445cbd7d45e032ff610bc5fa3be85fc4
Andy Dufilie <andy.dufilie@gmail.com>
2017-03-08 16:34:04 -0500

    Update tests

--- README.md
+++ README.md
@@ -2,20 +2,11 @@
 
 TypeScript example:
 ```
-type XObject = {origin: Foo, x: number};
+type XObject = {x: number};
 type ResultObject = {result: string};
 
 class Foo
 {
-	@memoize
-	makeXObject(value:number):XObject
-	{
-		return {
-			origin: this,
-			x: value
-		};
-	}
-
 	// used as a decorator
 	@memoize
 	bar(x: XObject, y: string, z?:number):ResultObject
@@ -45,14 +36,12 @@ function assert(shouldBeTrue:boolean, msg:string)
 
 let foo = new Foo();
 let foo2 = new Foo();
-let x = foo.makeXObject(123);
+let x = {x: 123};
 let bar = foo.bar(x, 'y');
-let bar2 = foo2.bar(x, 'y');
 let baz = foo.baz(x);
 
-assert(x === foo.makeXObject(123), 'memoized makeXObject');
 assert(bar === foo.bar(x, 'y'), 'memoized bar');
-assert(bar !== bar2, 'memoized per instance');
+assert(bar !== foo2.bar(x, 'y'), 'memoized per instance');
 assert(bar === baz, "foo.y === 'y' when baz was created");
 assert(baz === foo.baz(x), 'memoized baz');
 foo.y = 'yy';
--- test/memoize.test.ts
+++ test/memoize.test.ts
@@ -1,23 +1,14 @@
 import {assert} from 'chai';
 import memoize from "../src/memoize";
 
-type XObject = {origin: Foo, x: number};
+type XObject = {x: number};
 type ResultObject = {result: string};
 
 class Foo
 {
-	@memoize
-	makeXObject(value:number):XObject
-	{
-		return {
-			origin: this,
-			x: value
-		};
-	}
-
 	// used as a decorator
 	@memoize
-	bar(x: XObject, y: string, z?:number):ResultObject
+	bar(x: XObject, y: string, z?: number): ResultObject
 	{
 		return {
 			result: JSON.stringify({x, y, z})
@@ -27,7 +18,7 @@ class Foo
 	// used inline
 	baz = memoize({
 		getAdditionalArgs: () => [this.y],
-		function: (x:XObject):ResultObject => this.bar(x, this.y)
+		function: (x: XObject): ResultObject => this.bar(x, this.y)
 	});
 
 	y = 'y';
@@ -36,22 +27,22 @@ class Foo
 /**
  * Dummy test
  */
-describe('memoize', () => {
-  it('works as expected', () => {
-    let foo = new Foo();
-    let foo2 = new Foo();
-    let x = foo.makeXObject(123);
-    let bar = foo.bar(x, 'y');
-    let bar2 = foo2.bar(x, 'y');
-    let baz = foo.baz(x);
+describe('memoize', () =>
+{
+	it('works as expected', () =>
+	{
+		let foo = new Foo();
+		let foo2 = new Foo();
+		let x = {x: 123};
+		let bar = foo.bar(x, 'y');
+		let baz = foo.baz(x);
 
-    assert(x === foo.makeXObject(123), 'memoized makeXObject');
-    assert(bar === foo.bar(x, 'y'), 'memoized bar');
-    assert(bar !== bar2, 'memoized per instance');
-    assert(bar === baz, "foo.y === 'y' when baz was created");
-    assert(baz === foo.baz(x), 'memoized baz');
-    foo.y = 'yy';
-    assert(baz !== foo.baz(x), 'additional args for baz changed');
-    assert(foo.bar(x, 'y', 3) === foo.bar(x, 'y', 3), 'supports variable number of arguments');
-  })
+		assert(bar === foo.bar(x, 'y'), 'memoized bar');
+		assert(bar !== foo2.bar(x, 'y'), 'memoized per instance');
+		assert(bar === baz, 'foo.y === "y" when baz was created');
+		assert(baz === foo.baz(x), 'memoized baz');
+		foo.y = 'yy';
+		assert(baz !== foo.baz(x), 'additional args for baz changed');
+		assert(foo.bar(x, 'y', 3) === foo.bar(x, 'y', 3), 'supports variable number of arguments');
+	})
 })
